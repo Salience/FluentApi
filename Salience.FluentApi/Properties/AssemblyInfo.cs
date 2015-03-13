98a338283369196b07efdfcd3442fcaf4241b30a
Julien Patte <julien.patte@gmail.com>
2015-03-13 08:12:45 +0100

    updated AssemblyInfo

--- Salience.FluentApi/Properties/AssemblyInfo.cs
+++ Salience.FluentApi/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Salience.FluentApi")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A simple wrapper around RestSharp and Json.NET.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Salience")]
 [assembly: AssemblyProduct("Salience.FluentApi")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © Salience 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
